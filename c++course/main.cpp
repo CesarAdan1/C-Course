b4e0e28efd4bb07b293ddf1c68ee17e4e4fb4391
CesarAdan <whoclash@hotmail.com>
2020-02-28 18:31:51 -0600

    c++ in xcode

---
+++ c++course/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++course
+//
+//  Created by CesarAdan1 on 2/28/20.
+//  Copyright © 2020 CesarAdan1. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
