b8969b66aabd8e0a05114a6d21ff7d5e660c6a56
CesarAdan <whoclash@hotmail.com>
2020-03-06 12:58:02 -0600

    first

--- c++course/main.cpp
+++ c++course/main.cpp
@@ -8,8 +8,28 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
+    
+    int n1, n2;
+    int suma = 0;
+    int resta = 0;
+    int multiplicacion = 0;
+    float division = 0;
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Digita un número: "; cin>>n1;
+    cout << "Digita otro número: "; cin>>n2;
+    
+    suma = n1 + n2;
+    resta = n1 - n2;
+    multiplicacion = n1 * n2;
+    division = n1/n2;
+    
+    cout <<"\nEl resultado de la suma es " <<suma<<endl;
+    cout <<"El resultado de la resta es " <<resta<<endl;
+    cout <<"El resultado de la multiplicación es " <<multiplicacion<<endl;
+    cout <<"El resultado de la división es " <<division<<endl;
+    
     return 0;
 }
